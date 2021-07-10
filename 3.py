0da7eea1f5aace0d48f20522f3c5f798d312a278
Sayar Mandal <52607268+SayarGitHub@users.noreply.github.com>
2021-07-10 22:01:41 +0530

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,28 @@
+# range() is a generator. List comprehension saves entire list to memory.
+# So, generally list comprehensions are memory heavy but fast to iterate over.
+# Whereas generators are more memory efficient, but slow to iterate over.
+
+
+x = [i for i in range(5)]  # list comprehension
+print("a", x)
+y = (i for i in range(5))  # generator expression
+print("b", y)
+
+for i in y:
+    print("c", i)
+
+
+def div_five(num):
+    return num % 5 == 0
+
+
+input_list = [1, 5, 10, 15, 20, 7, 9, 3, 9]
+result = (i for i in input_list if div_five(i))
+print("d", result)
+[print("e", i) for i in result]  # basically acts as one-line for loop here
+
+result = [i for i in input_list if div_five(i)]
+print("f", result)
+[print("g", i) for i in result]
+
+[[print(i,ii) for ii in range(5)] for i in range(5)] #nested loops usinng list comprehension
