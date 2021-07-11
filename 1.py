8b61e69d8533d1d7f80fee859d76b698cd36a076
Sayar Mandal <52607268+SayarGitHub@users.noreply.github.com>
2021-07-11 16:41:18 +0530

    .join( )
    Explains why .join( ) is faster

--- 1.py
+++ 1.py
@@ -1,9 +1,13 @@
+# .join() is much faster than string concatenation
+
 names = ["James", "owen", "Chimi", "Klei"]
-print(', '.join(names))
+print(", ".join(names))
 
 import os
+
 location = "C:\\Users\\Sayar\\Tutorials\\Intermediate-Python"
 file_name = "example.txt"
 
 with open(os.path.join(location, file_name)) as f:
-    print(f.read()) 
+    print(f.read())
+
