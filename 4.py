48fef5b723932f3ac68210fb191b8a9a3512008e
Sayar Mandal <52607268+SayarGitHub@users.noreply.github.com>
2021-07-11 16:43:35 +0530

    enumerate( )
    Explains how to use enumerate

--- 4.py
+++ 4.py
@@ -1,6 +1,10 @@
+# Use enumerate to create an enumerate object containing tuples of index and
+# value, which can be iterated over.
+
 test_list = ["left", "right", "top", "bottom"]
 
 [print(i, j) for i, j in enumerate(test_list)]
 
 new_dict = dict(enumerate(test_list))
 print(new_dict)
+print(enumerate(test_list))
