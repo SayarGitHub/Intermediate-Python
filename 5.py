41654750935a45b59a04274bb31a05d7046c55a7
Sayar Mandal <52607268+SayarGitHub@users.noreply.github.com>
2021-07-11 16:44:05 +0530

    zip( )
    Explains how to use zip( )

--- 5.py
+++ 5.py
@@ -1,3 +1,5 @@
+# Use zip to combine elements at same index in different lists into a iterator
+# of tuples.
 x = [1, 5, 9, 7]
 y = [3, 5, 9, 1]
 z = ["a", "d", "f", "r"]
@@ -8,16 +10,17 @@ for a, b, c in zip(x, y, z):
 # print(list(zip(x, y, z)))
 # print(dict(zip(x, z)))
 
+# -----------------------------------------------------------------------------#
 
 # For loop temporary variable weirdness
 [print(x, y) for x, y in zip(x, y)]
 print(x)
-# Even though we used x and y as the temporary variables in list
-# comprehension to iterate over the zip object, the original x and y are intact.
+# Even though we used x and y as the temporary variables in list comprehension
+# to iterate over the zip object, the original x and y are intact.
 
 for x, y in zip(x, y):
     print(x, y)
 print(x)
-# But if we use x and y as temporary variables in for loop
-# the original x and y valures are overwritten.
+# But if we use x and y as temporary variables in for loop the original x and y
+# valures are overwritten.
 
